668444b976b957b8ca5ac66551117610c79ddb6c
xintian.wang23@gmail.com <xintian.wang23@gmail.com>
2019-04-26 17:25:35 -0400

    [New] Add Product class and IProductsRepository

---
+++ Wxt.OnlineSportStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wxt.OnlineSportStore.Domain.Entities;
+
+namespace Wxt.OnlineSportStore.Domain.Abstract
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
---
+++ Wxt.OnlineSportStore.Domain/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Wxt.OnlineSportStore.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please enter product's name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter product's description")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please specify product's category")]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(0.01, 100000, ErrorMessage = "Please enter a positive price for product")]
+        public decimal Price { get; set; }
+    }
+}
