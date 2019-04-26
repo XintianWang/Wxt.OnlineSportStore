8f7bfbd2f256a1f7a32bea4a7ce0d683be151d53
xintian.wang23@gmail.com <xintian.wang23@gmail.com>
2019-04-26 17:06:38 -0400

    Create Solution 1 [New] Add WebApp, Domain, UnitTests and DebugConsole projects 2 [Mod] Add references between projects 3 [Mod] Add Mock, AutoFac, AutoFac mvc5, BootStrap, jquery, Entity framework

---
+++ Wxt.OnlineSportStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Wxt.OnlineSportStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Wxt.OnlineSportStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("815428a7-8f7f-40fa-ade6-217ee95da341")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
